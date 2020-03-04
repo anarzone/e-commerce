ecf6f671d1c2489b3f96733291657b526d2d2c5d
anarzone <haqverdiyevanar@gmail.com>
2020-03-04 22:06:55 +0400

    created product and review factories

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -7,6 +7,10 @@ use Faker\Generator as Faker;
 
 $factory->define(Product::class, function (Faker $faker) {
     return [
-        //
+        "name" => $faker->name,
+        "details" => $faker->paragraph,
+        "price" => $faker->randomFloat(3, 10, 5000),
+        "stock" => $faker->numberBetween(1,500),
+        "discount" => $faker->numberBetween(10, 75),
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Product;
+use App\Review;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        factory(Product::class, 100)->create();
+        factory(Review::class, 1550)->create();
     }
 }
